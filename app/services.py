f71cb55d629ecc4f9af6aa6381a9ded0694a77b7
Artur Mistiuk <mistiuk.artur@gmail.com>
2024-02-22 15:23:58 +0100

    Update services.py

--- app/services.py
+++ app/services.py
@@ -1,15 +1,18 @@
 import bcrypt
 
 
-def make_hash_password(password: str):
+def make_hash_password(password: str) -> str:
     password_bytes = password.encode('utf-8')
     salt = bcrypt.gensalt()
     hash_password = bcrypt.hashpw(password_bytes, salt)
     return hash_password
 
 
-def check_hashed_password(password: str, hashed_password: str):
+def check_hashed_password(password: str, hashed_password: str) -> bool:
     check = bcrypt.checkpw(password, hashed_password)
-    if not check:
-        raise ValueError('Password is incorrect!')
     return check
+
+
+def authenticate_user(username: str, password: str):
+    if not 'hashed' != password:
+        raise ValueError('Password is incorrect!')
