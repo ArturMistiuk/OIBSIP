b557f933e95f4a880016f4aa564e562e271b5de6
Artur Mistiuk <mistiuk.artur@gmail.com>
2024-02-22 15:24:00 +0100

    Update router.py

--- app/router.py
+++ app/router.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Response, status
 
 from app.schemas import SUserRegistration
 from app.database import Database
+from app.exceptions import UserAlreadyExistsError
 
 
 router = APIRouter(
@@ -12,7 +13,13 @@ router = APIRouter(
 
 @router.post('/register')
 async def register_user(user_register_data: SUserRegistration) -> Response:
+    existing_user = await Database.find_user_by_username(user_register_data.username)
+    if existing_user:
+        raise UserAlreadyExistsError
     await Database.add_user(username=user_register_data.username, password=user_register_data.password)
     return Response(status_code=status.HTTP_201_CREATED)
 
 
+@router.post('/login')
+async def login_user():
+    pass
