d9f81da9de76c4fc04bb2b110328d1904ab3cce1
Artur Mistiuk <mistiuk.artur@gmail.com>
2024-02-23 21:44:30 +0100

    Update router.py

--- app/router.py
+++ app/router.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, Response, status
 
-from app.schemas import SUserRegistration
+from app.schemas import SUserRegistration, SUserLogin
 from app.database import Database
-from app.exceptions import UserAlreadyExistsError
+from app.exceptions import UserAlreadyExistsError, IncorrectUsernameOrPasswordError
+from app.auth import make_hash_password, authenticate_user
 
 
 router = APIRouter(
@@ -12,14 +13,18 @@ router = APIRouter(
 
 
 @router.post('/register')
-async def register_user(user_register_data: SUserRegistration) -> Response:
-    existing_user = await Database.find_user_by_username(user_register_data.username)
+async def register_user(user_data: SUserRegistration) -> Response:
+    existing_user = await Database.find_user_by_username(user_data.username)
     if existing_user:
         raise UserAlreadyExistsError
-    await Database.add_user(username=user_register_data.username, password=user_register_data.password)
+    hashed_password = make_hash_password(user_data.password)
+    await Database.add_user(username=user_data.username, hashed_password=hashed_password)
     return Response(status_code=status.HTTP_201_CREATED)
 
 
 @router.post('/login')
-async def login_user():
-    pass
+async def login_user(user_data: SUserLogin):
+    user = await authenticate_user(user_data.username, user_data.password)
+    if not user:
+        raise IncorrectUsernameOrPasswordError
+    return user
