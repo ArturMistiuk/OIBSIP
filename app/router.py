5925c0d53dd1f14eadd0b26d831f2d59958e623f
Artur Mistiuk <mistiuk.artur@gmail.com>
2024-02-27 23:48:33 +0100

    Update router.py

--- app/router.py
+++ app/router.py
@@ -1,9 +1,10 @@
-from fastapi import APIRouter, Response, status
+from fastapi import APIRouter, Response, status, Depends
 
 from app.schemas import SUserRegistration, SUserLogin
-from app.database import Database
+from app.database import Database, User
 from app.exceptions import UserAlreadyExistsError, IncorrectUsernameOrPasswordError
-from app.auth import make_hash_password, authenticate_user
+from app.auth import make_hash_password, authenticate_user, create_access_token
+from app.dependencies import get_current_user
 
 
 router = APIRouter(
@@ -23,8 +24,16 @@ async def register_user(user_data: SUserRegistration) -> Response:
 
 
 @router.post('/login')
-async def login_user(user_data: SUserLogin):
+async def login_user(response: Response, user_data: SUserLogin):
     user = await authenticate_user(user_data.username, user_data.password)
+    print(user, 'USER')
     if not user:
         raise IncorrectUsernameOrPasswordError
+    access_token = create_access_token({'sub': str(user.id)})
+    response.set_cookie('access_token', access_token, httponly=True)
+    return {'access_token': access_token}
+
+
+@router.post('/secured_page')
+async def secured_page(user: User = Depends(get_current_user)):
     return user
