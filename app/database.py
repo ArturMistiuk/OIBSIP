6019900964236952610f1f61e2f9c6312cdd2c2b
Artur Mistiuk <mistiuk.artur@gmail.com>
2024-02-28 16:32:53 +0100

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from threading import Lock
 from typing import NoReturn, Union
-
+from app.exceptions import UserIsNotPresentError
 
 class DatabaseMeta(type):
     _instances = {}
@@ -18,39 +18,37 @@ class DatabaseMeta(type):
 
 @dataclass
 class User:
+    id: int
     username: str
     password: str
-    id_counter: int = 0
-    id: int = field(init=False)
-
-    def __post_init__(self):
-        self.id = User.id_counter
-        User.id_counter += 1
 
 
 class Database(metaclass=DatabaseMeta):
     users = {}
     _lock: Lock = Lock()
+    id_counter: int = 0
 
     @staticmethod
     async def add_user(username: str, hashed_password: str) -> NoReturn:
         new_user = User(
+            id=Database.id_counter,
             username=username,
             password=hashed_password
         )
         with Database._lock:
+            Database.id_counter += 1
             Database.users[new_user.id] = (new_user.username, new_user.password)
+
 
     @staticmethod
     async def find_user_by_username(username: str) -> Union[User, None]:
-        for stored_username, stored_password in Database.users.values():
-            if username == stored_username:
-                return User(username=stored_username, password=stored_password)
+        for user_id, (user_username, user_password) in Database.users.items():
+            if username == user_username:
+                return User(id=user_id, username=user_username, password=user_password)
         return None
 
     @staticmethod
     async def find_user_by_id(user_id: int) -> Union[User, None]:
-        print(Database.users)
-        print(Database.users.get(user_id))
-
-
+        user_data: tuple = Database.users.get(user_id)
+        if user_data:
+            return User(id=user_id, username=user_data[0], password=user_data[1])
